574aaf299ba4db6f0927b44daaa6bce281ec81a0
S.Panfilov <se-panfilov@ya.ru>
2015-05-22 14:06:44 +0800

    Extended structure

---
+++ gulpfile.js
@@ -0,0 +1,148 @@
+var gulp = require('gulp');
+var concat = require('gulp-concat');
+var rename = require('gulp-rename');
+var uglify = require('gulp-uglify');
+var jade = require('gulp-jade');
+var sourcemaps = require('gulp-sourcemaps');
+var watch = require('gulp-watch');
+var jshint = require('gulp-jshint');
+var changed = require('gulp-changed');
+var ngAnnotate = require('gulp-ng-annotate');
+var stylus = require('gulp-stylus');
+var nib = require('nib');
+var connect = require('gulp-connect');
+var minifyHTML = require('gulp-minify-html');
+var minifyCss = require('gulp-minify-css');
+
+var src = {
+    styles: {
+        demo: 'demo',
+        src: [
+            'src/styles/**/*.styl'
+        ]
+    },
+    jade: {
+        demo: 'demo',
+        src: [
+
+            'src/**/*.jade',
+            'src/*.jade'
+        ]
+    }
+    ,
+    js: 'src/**/*.js'
+};
+
+var dest = {
+    dist: 'dist',
+    src: 'src',
+    demo: 'demo'
+};
+
+gulp.task('lint', function () {
+    return gulp.src(src.js)
+        .pipe(jshint({
+            globalstrict: true,
+            strict: false,
+            globals: {
+                angular: true
+            }
+        }))
+        .pipe(jshint.reporter('jshint-stylish'));
+});
+
+gulp.task('js', function () {
+    return gulp.src([src.js])
+        .pipe(changed(dest.staticDir))
+        .pipe(concat('app.js'))
+        .pipe(ngAnnotate({remove: true, add: true, single_quotes: true}))
+        .on('error', console.log)
+        .pipe(gulp.dest(dest.staticDir))
+        .pipe(sourcemaps.init())
+        .pipe(uglify())
+        .pipe(rename({basename: 'app.min'}))
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest(dest.staticDir))
+        ;
+});
+
+gulp.task('jade_demo', function () {
+    return gulp.src(src.jade.demo)
+        .pipe(changed(dest.demo, {extension: '.html'}))
+        .pipe(jade({pretty: false}))
+        .on('error', console.log)
+        .pipe(minifyHTML({
+            empty: true,
+            spare: true
+        }))
+        .pipe(gulp.dest(dest.demo));
+});
+
+gulp.task('jade_src', function () {
+    return gulp.src(src.jade.src)
+        .pipe(changed(dest.dist, {extension: '.html'}))
+        .pipe(jade({pretty: false}))
+        .on('error', console.log)
+        .pipe(minifyHTML({
+            empty: true,
+            spare: true
+        }))
+        .pipe(gulp.dest(dest.dist));
+});
+
+gulp.task('stylus_demo', function () {
+    return gulp.src(src.styles.demo, {base: 'demo'})
+        .pipe(concat('demo.styl'))
+        .pipe(stylus({use: [nib()], compress: true}))
+        .on('error', console.log)
+        .pipe(minifyCss())
+        .pipe(gulp.dest(dest.demo));
+});
+
+gulp.task('stylus_src', function () {
+    return gulp.src(src.styles.src, {base: 'src'})
+        .pipe(concat('bdate.styl'))
+        .pipe(stylus({use: [nib()], compress: true}))
+        .on('error', console.log)
+        .pipe(minifyCss())
+        .pipe(gulp.dest(dest.dist));
+});
+
+gulp.task('jade', function () {
+    gulp.start('jade_demo');
+    gulp.start('jade_src');
+});
+
+gulp.task('stylus', function () {
+    gulp.start('stylus_demo');
+    gulp.start('stylus_src');
+});
+
+gulp.task('watch', function () {
+    gulp.watch(src.jade.demo, ['jade_demo']);
+    gulp.watch(src.jade.src, ['jade_src']);
+
+    gulp.watch(src.styles.demo, ['stylus_demo']);
+    gulp.watch(src.styles.src, ['stylus_src']);
+
+    gulp.watch(src.js, ['js']);
+});
+
+gulp.task('build', function () {
+    gulp.start('js');
+    gulp.start('jade');
+    gulp.start('stylus');
+});
+
+gulp.task('webserver', function () {
+    connect.server({
+        root: [__dirname],
+        port: 8001,
+        livereload: true
+    });
+});
+
+gulp.task('default', function () {
+    gulp.start('build');
+    gulp.start('watch');
+});
