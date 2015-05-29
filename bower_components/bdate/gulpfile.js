b71b82bb4575698a7f2fca750d7b28d9d7c24699
S.Panfilov <se-panfilov@ya.ru>
2015-05-29 17:53:03 +0800

    Fixes

---
+++ bower_components/bdate/gulpfile.js
@@ -0,0 +1,39 @@
+var gulp = require('gulp');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var jade = require('gulp-jade');
+var stylus = require('gulp-stylus');
+var nib = require('nib');
+var connect = require('gulp-connect');
+var minifyCss = require('gulp-minify-css');
+
+gulp.task('jade', function () {
+    return gulp.src('./*.jade')
+        .pipe(jade({pretty: true}))
+        .on('error', console.log)
+        .pipe(gulp.dest('./'));
+});
+
+gulp.task('stylus', function () {
+    return gulp.src('./styles/*.styl')
+        .pipe(concat('index.styl'))
+        .pipe(stylus({use: [nib()], compress: false}))
+        .on('error', console.log)
+        .pipe(minifyCss())
+        .pipe(gulp.dest('./'));
+});
+
+gulp.task('watch', function () {
+    gulp.watch('./*.jade', ['jade']);
+    gulp.watch('./styles/*.styl', ['stylus']);
+});
+
+gulp.task('build', function () {
+    gulp.start('jade');
+    gulp.start('stylus');
+});
+
+gulp.task('default', function () {
+    gulp.start('build');
+    gulp.start('watch');
+});
