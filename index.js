6ec5c87a0f3cd137d79d55b615a2ca3fdc6e7b71
S.Panfilov <se-panfilov@ya.ru>
2015-06-17 13:44:19 +0800

    Fixes

--- index.js
+++ index.js
@@ -860,7 +860,7 @@ angular.module('demo', [
                     "12": {"days_total": 31, "start_day": 3}
                 }
             },
-            "today":  {"date": 1433909084, "month": 6, "day": 10, "day_of_week": 3, "year": 2015},
+            "today": {"date": 1433909084, "month": 6, "day": 10, "day_of_week": 3, "year": 2015},
             "format": "yyyy-MM-dd"
         };
 
@@ -874,6 +874,30 @@ angular.module('demo', [
             $scope.delayedDemoData = copy($scope.demoData);
         }, 2000);
 
+        $scope.localizedDays = [
+            {name: 'Понедельник', short: 'Пн'},
+            {name: 'Вторник', short: 'Вт'},
+            {name: 'Среда', short: 'Ср'},
+            {name: 'Четверг', short: 'Чт'},
+            {name: 'Пятница', short: 'Пт'},
+            {name: 'Суббота', short: 'Сб'},
+            {name: 'Воскресенье', short: 'Вс'}
+        ];
+
+        $scope.localizedMonth = {
+            1: {name: 'Январь', short: 'Янв'},
+            2: {name: 'Февраль', short: 'Фев'},
+            3: {name: 'Март', short: 'Март'},
+            4: {name: 'Апрель', short: 'Май'},
+            5: {name: 'Май', short: 'Май'},
+            6: {name: 'Июнь', short: 'Июнь'},
+            7: {name: 'Июль', short: 'Июль'},
+            8: {name: 'Август', short: 'Авг'},
+            9: {name: 'Сентябрь', short: 'Сент'},
+            10: {name: 'Октябрь', short: 'Окт'},
+            11: {name: 'Ноябрь', short: 'Ноя'},
+            12: {name: 'Декабрь', short: 'Дек'}
+        };
 
         $scope.resultModel = '';
         $scope.resultDelayedModel = '';
