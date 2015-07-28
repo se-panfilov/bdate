637270b78a4b576f2412a38687367971ac7a7da4
S.Panfilov <se-panfilov@ya.ru>
2015-07-28 10:55:31 +0800

    Fixes

--- index.js
+++ index.js
@@ -6,17 +6,54 @@ angular.module('demo', [
 
     .controller('DemoPageCtrl', function ($scope, $http) {
 
+        $scope.settings = {
+            "week": ["Пн", "Вт", "Ср", "Чт", "Пт", "Сб", "Вс"],
+            "today": {
+                "day": 3,
+                "month": 1,
+                "year": 2010
+            },
+            "format": "dd-MM-yyyy",
+            "range_delimiter": "--"
+        };
 
         $scope.refreshData = function (m, y) {
-            var params = (month && year) ? month + '-' + year : null;
+            var defVal = "1-2010";
+            var params = (m && y) ? m + '-' + y : defVal;
             var url = 'http://localhost:3000/resp/' + params;
 
             $http.get(url).success(function (data) {
-                $scope.demoData = data;
+                $scope.source = data;
             }).error(function (data, status) {
                 console.error(status);
             });
 
+        };
+
+        $scope.refreshStartData = function (m, y) {
+            var defVal = "1-2010";
+            var params = (m && y) ? m + '-' + y : defVal;
+            var url = 'http://localhost:3000/resp/' + params;
+
+            $http.get(url).success(function (data) {
+                $scope.startSource = data;
+                $scope.startSource.comment = 'start';
+            }).error(function (data, status) {
+                console.error(status);
+            });
+        };
+
+        $scope.refreshEndData = function (m, y) {
+            var defVal = "1-2010";
+            var params = (m && y) ? m + '-' + y : defVal;
+            var url = 'http://localhost:3000/resp/' + params;
+
+            $http.get(url).success(function (data) {
+                $scope.endSource = data;
+                $scope.endSource.comment = 'end';
+            }).error(function (data, status) {
+                console.error(status);
+            });
         };
 
         //$scope.delayedDemoData = {};
